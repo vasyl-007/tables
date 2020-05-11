ef15bfeee6b0f6b6669d81946c75e41d7e7dc806
Vasyl-007 <vasyl.manyo@gmail.com>
2020-05-11 18:03:28 +0300

    added new functions

--- TestTask_9x9-master/index.js
+++ TestTask_9x9-master/index.js
@@ -6,38 +6,25 @@ const bigCube = {
   yMin: 1,
   yMax: 3,
 };
-
-const arrows = [
-  { 1: "&#8593;", description: "вверх" },
-  { 2: "&#8595;", description: "вниз" },
-  { 3: "&#8594;", description: "направо" },
-  { 4: "&#8592;", description: "налево" },
-];
-
 const initialSmallCube = {
   x: 1,
   y: 1,
   // positionArrow: 'вверх спецсимвол'
 };
-
-// const a = [10, 20, 30];
-const coordXY = [
-  [1, 1],
-  [1, 2],
-  [1, 3],
-  [2, 1],
-  [2, 2],
-  [2, 3],
-  [3, 1],
-  [3, 2],
-  [3, 3],
+const allArrows = [
+  { id: 1, direction: "&#8593;", description: "вверх" },
+  { id: 2, direction: "&#8595;", description: "вниз" },
+  { id: 3, direction: "&#8594;", description: "направо" },
+  { id: 4, direction: "&#8592;", description: "налево" },
 ];
 
-console.log(coordXY.indexOf([1])); // 10
+console.log("allArrows", allArrows);
+console.log("currentId", allArrows[0].id);
 
 const RANDOM_POSITION = 9;
 const ARROWS_LIST = 5;
 const arrowList = document.getElementById("arrows");
+
 
 function getRandomPosition(RANDOM_POSITION) {
   let initialPosition = Math.floor(Math.random(RANDOM_POSITION) * 10);
@@ -53,19 +40,36 @@ getRandomPosition();
 
 const getRandomArrows = (bigCube, initialSmallCube) => {
   let arrows = [];
+  const min_arrowList = 1;
+  function invokeArrow() {
+    let result =
+      Math.floor(Math.random() * (ARROWS_LIST - min_arrowList)) + min_arrowList;
+    return result;
 
-  if (initialSmallCube.x === bigCube.xMin) {
-    // x уменьшать нельзя
+    // (case 1)
+    if (
+      initialSmallCube.x === bigCube.xMin &&
+      initialSmallCube.y === bigCube.yMin
+    ) {
+      let curArrow;
+
+      do {
+        curArrow = invokeArrow();
+      } while (curArrow === allArrows[3].id || curArrow === allArrows[1].id);
+      // цикл буде выполняться до тех пор, пока значения будут 4 либо 2
+
+      return arrows.push(curArrow);
+    }
   }
-  if (initialSmallCube.x === bigCube.xMax) {
-    // x увеличивать нельзя
-  }
-  if (initialSmallCube.y === bigCube.yMin) {
-    // y уменьшать нельзя
-  }
-  if (initialSmallCube.y === bigCube.yMax) {
-    // y увеличивать нельзя
-  }
+  // if (initialSmallCube.x === bigCube.xMax) {
+  //   // x увеличивать нельзя
+  // }
+  // if (initialSmallCube.y === bigCube.yMin) {
+  //   // y уменьшать нельзя
+  // }
+  // if (initialSmallCube.y === bigCube.yMax) {
+  //   // y увеличивать нельзя
+  // }
 
   // (1) Xmin Ymin 	влево и вниз нельзя 	(4,2)
   // (2) Xmin Ynorm	влево нельзя		(4)
@@ -79,7 +83,6 @@ const getRandomArrows = (bigCube, initialSmallCube) => {
   // (8) Xmax Ynorm	вправо нельзя		(3)
   // (9) Xmax Ymax	вправо и вверх нельзя	(3,1)
 
-  const min_arrowList = 1;
   arrows.push(
     Math.floor(Math.random() * (ARROWS_LIST - min_arrowList)) + min_arrowList
   );
@@ -96,19 +99,23 @@ const getRandomArrows = (bigCube, initialSmallCube) => {
     switch (arrow) {
       case 1:
         console.log("Вверх");
-        action = "&#8593;";
+        action = allArrows[0].direction;
+        // action = "&#8593;";
         break;
       case 2:
         console.log("Вниз");
-        action = "&#8595;";
+        action = allArrows[1].direction;
+        // action = "&#8595;";
         break;
       case 3:
         console.log("Направо");
-        action = "&#8594;";
+        action = allArrows[2].direction;
+        // action = "&#8594;";
         break;
       case 4:
         console.log("Налево");
-        action = "&#8592;";
+        action = allArrows[3].direction;
+        // action = "&#8592;";
         break;
       default:
         console.log("Invalid subscription type");
